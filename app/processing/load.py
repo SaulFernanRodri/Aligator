d4b303a720139409baeb4d94db41395e409b1254
Saúl <saul.fernanrodri@gmail.com>
2024-03-28 15:37:42 +0100

    .

--- app/processing/load.py
+++ app/processing/load.py
@@ -4,16 +4,9 @@ from sklearn.preprocessing import MinMaxScaler
 
 def normalize_dataframe(file_path):
     df = pd.read_csv(file_path, sep=',')
-    # Seleccionar columnas que no se quieren normalizar
     cols_to_exclude = ['Timestep', 'Sector']
-
-    # Seleccionar las columnas que se van a normalizar
     cols_to_normalize = df.columns.difference(cols_to_exclude)
-
-    # Inicializar el normalizador
     scaler = MinMaxScaler()
-
-    # Normalizar los datos y ajustar al rango deseado
     df[cols_to_normalize] = scaler.fit_transform(df[cols_to_normalize])
     pickle.dump(scaler, open('scaler.pkl', 'wb'))
     return df
