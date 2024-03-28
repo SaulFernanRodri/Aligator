0e23f6a14b10c79441891530423f1939c5f2d37e
Saúl <saul.fernanrodri@gmail.com>
2024-03-28 15:35:36 +0100

    .

--- app/models/test.py
+++ app/models/test.py
@@ -1,9 +1,7 @@
 from sklearn.metrics import mean_squared_error, r2_score
 def test(model, X_test, y_test):
-    # Predecir en el conjunto de prueba
     y_pred_test = model.predict(X_test)
 
-    # Calcular métricas de regresión
     mse = mean_squared_error(y_test, y_pred_test)
     r2 = r2_score(y_test, y_pred_test)
 
