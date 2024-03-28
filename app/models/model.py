b36117b2f886dbb4418e6c001d2f432cad7dc925
Saúl <saul.fernanrodri@gmail.com>
2024-03-28 15:34:24 +0100

    .

--- app/models/model.py
+++ app/models/model.py
@@ -2,7 +2,7 @@ from sklearn.model_selection import train_test_split
 
 def model(df):
     X = df.drop(columns=['Timestep', 'Sector', 'Target CellSmutans', 'Target CellSmitis', 'Target Macromoleculeahl'])
-    y = df['Target Macromoleculeahl']
+    y = df['Target CellSmutans']
 
     X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
